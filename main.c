aaf2e74f5a0d03f95aad9ae78cbafe9a4527afa8
helloyewon36 <helloyewon36@gmail.com>
2025-10-16 15:36:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(int argc, char*argv[])
 {
-    int i;
-    
-    for(i=0;i<5;i++)
-    {
-                   static int temp=1;
-                    printf("temp=%d\n", temp);
-                    temp++;
-                    }
+  int i=10;
+  
+  printf("함수 호출 전i=%d\n",i);
+  i=inc(i);
+  printf("함수 호출 후i=%d\n",i);
   system("PAUSE");	
   return 0;
+}
+int inc (int counter)
+{
+    counter++;
+    return counter; 
+  
+  
+  
 }
